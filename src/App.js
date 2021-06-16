2620fb8b74dfd0c6aa390161609ab5e75b36e1c7
pavan <pavankumarlakku73@gmail.com>
2021-06-17 01:36:53 +0530

    Task is completed

--- src/App.js
+++ src/App.js
@@ -1,23 +1,22 @@
 import logo from './logo.svg';
 import './App.css';
-
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import Login from "./Login";
+import Dashboard from "./Dashboard";
 function App() {
+ 
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Router>
+        <div>
+          <Route exact path="/">
+            <Login />
+          </Route>
+          <Route path="/dashboard">
+            <Dashboard />
+          </Route>
+        </div>
+      </Router>,
     </div>
   );
 }
---
+++ src/Login.js
@@ -0,0 +1,60 @@
+import React, { useEffect, useState } from 'react';
+import { Input, Button, message } from 'antd';
+//import {useState} from "react";
+import { UserOutlined } from '@ant-design/icons';
+import { EyeInvisibleOutlined, EyeTwoTone } from '@ant-design/icons';
+import "antd/dist/antd.css";
+import "./Login.css"
+const Login = () => {
+    const [disable1,setDisable] = useState(true);
+    const [name,setName] = useState();
+    const [password, setPassword] = useState();
+    
+    const onChange1 = (e) => {
+        if ("username" == e.target.name){
+            setName(e.target.value);
+        } else {
+            setPassword(e.target.value);
+        }
+        if (name && e.target.name == "password" || "username" == e.target.name && password ){
+            if (e.target.value.length == 0){
+                setDisable(true);
+            }else {
+            setDisable(false);
+            }
+        }
+    }
+    const onSubmit = () => {
+        if (name == "hruday@gmail.com" && password == "hruday123"){
+            message.success('successfully logged in');
+            window.location.href = '/dashboard'
+        } else {
+            //alert("password incorrect");
+            message.error('password or username is incorrect');
+        }
+    }
+    console.log(disable1,'disable1');
+    return (
+        <div><div style={{ margin: "50px auto", padding: "50px 75px", backgroundColor: "#f6fbfd", height: "300px", width: "450px", borderRadius: "10px"}}>
+        <h2>USER LOGIN</h2>
+        <Input size="large" name="username" onChange={onChange1} placeholder="enter username" prefix={<UserOutlined />} allowClear/>
+        <br />
+        <br />
+
+        <Input.Password
+            prefix={<UserOutlined />}
+            placeholder="enter password" 
+            name="password"
+            size="large"
+            onChange={onChange1}
+            iconRender={visible => (visible ? <EyeTwoTone /> : <EyeInvisibleOutlined />)}
+        />
+        <br />
+        <br />
+        <Button className="LoginButton" type="primary" disabled={disable1} onClick={onSubmit}>
+            LOGIN
+        </Button>   
+    </div>
+    </div>)
+}
+export default Login;
